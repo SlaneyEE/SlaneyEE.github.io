9909a7ebabcf48ed35975274baa177be310740bd
remojansen <remo.jansen@wolksoftware.com>
2026-01-30 10:51:32 +0000

    1.0

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -8,7 +8,7 @@ export function Section({
   id,
 }: { children: React.ReactNode; id: string }) {
   return (
-    <section className="w-full text-center text-gray-600 mb-50 px-5 scroll-mt-30" id={id}>
+    <section className="w-full text-center text-gray-400 mb-50 px-5 scroll-mt-30" id={id}>
       {children}
     </section>
   )
